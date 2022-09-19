ac9838126753dbda773375041f852b4c8f1c1f87
lucianovilela <lucianovilela@gmail.com>
2022-09-19 23:28:11 +0000

    wip

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 var session = require("express-session");
 var bodyParser = require("body-parser");
+require('dotenv').config();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,25 +1,30 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const Root = () => <div>Root</div>;
 
 const Team = () => <div>Team</div>;
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "team",
-        element: <Team />
-      }
-    ]
-  }
-]);
+
+
 
 export default function () {
+
+      const router = createBrowserRouter([
+        {
+          path: "/",
+          element: <Root />,
+          children: [
+            {
+              path: "team",
+              element: <Team />
+            }
+          ]
+        }
+      ]);
+
   return <RouterProvider router={router} />;
+  //return <div>Funcionou</div>;
+
 }
 
 export { Root, Team };
